eabea512bdf642b59e4f2547646db4bc3c26aac0
dejwid02 <dejwid02@wp.pl>
2018-06-22 23:45:38 +0200

    init electron file explorer app

---
+++ fileexplorer/main.js
@@ -0,0 +1,14 @@
+'use strict'
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow = null;
+
+app.on('window-all-closed', ()=> {app.quit();});
+
+app.on('ready', ()=>{
+    mainWindow = new BrowserWindow();
+    mainWindow.loadURL(`file://${app.getAppPath()}/index.html`);
+    mainWindow.on('closed', ()=> {mainWindow = null;});
+});
