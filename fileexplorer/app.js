386df4974b60a031bcbbb418e355a6cae91525bc
dejwid02 <dejwid02@wp.pl>
2018-07-26 22:24:19 +0200

    add functions to display different folders

--- fileexplorer/app.js
+++ fileexplorer/app.js
@@ -4,13 +4,7 @@ const userInterface = require('./userInterace')
 function main() {
     const folderPath = fileSystem.getUsersHomeFolder();
     userInterface.bindDocument(window);
-    fileSystem.getFilesInFolder(folderPath, (err, files) => {
-        if (err) {
-            return alert ('Sorry we could not load home folder')
-        }
-        fileSystem.inspectAndDescribeFiles(folderPath, files, userInterface.displayFiles)
-    });
-   
+    userInterface.loadDirectory(folderPath);  
 
 }
 main();
--- fileexplorer/userInterace.js
+++ fileexplorer/userInterace.js
@@ -1,5 +1,29 @@
 'use strict';
 let document;
+const fileSystem = require('./fileSystem');
+
+function displayFolder(folderPath){
+    document.getElementById('current-folder').innerText=folderPath;
+}
+
+function loadDirectory(folderPath) {
+    fileSystem.getFilesInFolder(folderPath, (err, files) => {
+        if (err) {
+            return alert('Sorry we could not load home folder');
+        }
+        fileSystem.inspectAndDescribeFiles(folderPath, files, userInterface.displayFiles);
+    });
+}
+function clearView(){
+    const mainArea = document.getElementById('main-area');
+    let firstChild = mainArea.firstChild();
+    while(firstChild) {
+        mainArea.removeChild(firstChild);
+        firstChild = mainArea.firstChild;
+    }
+}
+
+
 function displayFile(file) {
     const mainArea = document.getElementById("main-area");
     const template = document.querySelector('#item-template');
